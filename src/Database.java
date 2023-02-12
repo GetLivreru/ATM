ad23857805f81ccb72a4cb5100c8176e6678fda4
theadilet <110306539+GetLivreru@users.noreply.github.com>
2023-02-12 17:25:52 +0600

    Update Database.java

--- src/Database.java
+++ src/Database.java
@@ -1,2 +1,16 @@
+import java.sql.*;
 public class Database {
+  private Connection connection;
+
+    public void connect() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection("jdbc:postgresql://host:port/database", "username", "password");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+  
 }
