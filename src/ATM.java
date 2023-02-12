45c5231cb18188c0cdf2ff57fcef48bf0bb48eae
theadilet <110306539+GetLivreru@users.noreply.github.com>
2023-02-12 20:05:57 +0600

    Update ATM.java
    add scanner

--- src/ATM.java
+++ src/ATM.java
@@ -1,5 +1,17 @@
 public class ATM {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome! Please register!");
+        System.out.print("Login: ");
+        String login = scanner.nextLine();
+        String password;
+        while (true) {
+            System.out.print("Password: ");
+            password = scanner.nextLine();
+            if (password.length() >= 8 && hasNumber(password) && hasCapital(password) && hasSymbol(password)) {
+                break;
+            }
+            System.out.println("Password must be at least 8 characters long, contain at least one number, one capital letter, and one symbol.");
+        }
+        System.out.println("Thank you for registering, " + login + "!");
     }
-}
