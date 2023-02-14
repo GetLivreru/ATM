2af28bae28a99c8a5bd421713fa802d16dfcfb5a
AidanaIbrayeva <125108139+AidanaIbrayeva@users.noreply.github.com>
2023-02-14 21:31:04 +0600

    Update Operation.java

--- src/Operation.java
+++ src/Operation.java
@@ -1,2 +1,17 @@
 public class Operation {
-}
+    public void deposit(Account account, int amount) {
+        account.setBalance(account.getBalance() + amount);
+    }
+
+    public void withdraw(Account account, int amount) {
+        if (account.getBalance() >= amount) {
+            account.setBalance(account.getBalance() - amount);
+        } else {
+            throw new IllegalArgumentException("Insufficient funds");
+        }
+    }
+
+    public int checkBalance(Account account) {
+        return account.getBalance();
+    }
+} 
