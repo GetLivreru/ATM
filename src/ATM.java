07e40759227997bf7bf2a302589129b0c21ab8dc
AidanaIbrayeva <125108139+AidanaIbrayeva@users.noreply.github.com>
2023-02-14 21:36:37 +0600

    Update ATM.java

--- src/ATM.java
+++ src/ATM.java
@@ -1,19 +1,52 @@
 package basic.atm;
+
 import java.util.Scanner;
+
 public class ATM {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome! Please register!");
-        System.out.print("Login: ");
-        String login = scanner.nextLine();
-        String password;
+        Scanner input = new Scanner(System.in);
+        int balance = 1000; // starting balance
+        
+        // login
+        System.out.println("Enter your PIN:");
+        int pin = input.nextInt();
+        
+        // menu
         while (true) {
-            System.out.print("Password: ");
-            password = scanner.nextLine();
-            if (password.length() >= 8 && hasNumber(password) && hasCapital(password) && hasSymbol(password)) {
-                break;
+            System.out.println("Welcome to the ATM. What would you like to do?");
+            System.out.println("1. Check balance");
+            System.out.println("2. Withdraw money");
+            System.out.println("3. Deposit money");
+            System.out.println("4. Exit");
+            int choice = input.nextInt();
+            
+            switch (choice) {
+                case 1:
+                    System.out.println("Your balance is: " + balance);
+                    break;
+                case 2:
+                    System.out.println("Enter amount to withdraw:");
+                    int amount = input.nextInt();
+                    if (amount > balance) {
+                        System.out.println("Insufficient funds.");
+                    } else {
+                        balance -= amount;
+                        System.out.println("Withdrawn " + amount + ". Your new balance is " + balance);
+                    }
+                    break;
+                case 3:
+                    System.out.println("Enter amount to deposit:");
+                    int deposit = input.nextInt();
+                    balance += deposit;
+                    System.out.println("Deposited " + deposit + ". Your new balance is " + balance);
+                    break;
+                case 4:
+                    System.out.println("Thank you for using the ATM.");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice.");
             }
-            System.out.println("Password must be at least 8 characters long, contain at least one number, one capital letter, and one symbol.");
         }
-        System.out.println("Thank you for registering, " + login + "!");
     }
+}
+
