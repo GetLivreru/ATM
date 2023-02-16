b92a094a290629c311201da2fc1804243a6f1fe7
AidanaIbrayeva <125108139+AidanaIbrayeva@users.noreply.github.com>
2023-02-16 19:45:35 +0600

    Update Account.java

--- src/Account.java
+++ src/Account.java
@@ -1,10 +1,8 @@
-
 package basic.atm;
+
 public class Account implements BankOperations {
     private String firstName;
-
     private String lastName;
-
     private Integer balance;
 
     Account(String firstName, String lastName) {
@@ -14,36 +12,42 @@ public class Account implements BankOperations {
 
     public String generateCardNumber() {
         Integer length = 8;
-
         String passwordSet = "1234567890";
-
         char[] cardNumber = new char[length];
 
         for (int i = 0; i < length; i++) {
             int rand = (int) (Math.random() * passwordSet.length());
-
             cardNumber[i] = passwordSet.charAt(rand);
         }
 
         return new String(cardNumber);
     }
 
-    /*
-Pin code generation
-*/
     public String generatePincode() {
         Integer length = 4;
-
         String passwordSet = "1234567890";
-
-        char[] cardNumber = new char[length];
+        char[] pinCode = new char[length];
 
         for (int i = 0; i < length; i++) {
             int rand = (int) (Math.random() * passwordSet.length());
-
-            cardNumber[i] = passwordSet.charAt(rand);
+            pinCode[i] = passwordSet.charAt(rand);
         }
 
-        return new String(cardNumber);
+        return new String(pinCode);
+    }
+
+    public void deposit(Integer amount) {
+        this.balance += amount;
+    }
+
+    public void withdraw(Integer amount) {
+        if (this.balance < amount) {
+            throw new RuntimeException("Insufficient balance");
+        }
+        this.balance -= amount;
+    }
+
+    public Integer getBalance() {
+        return this.balance;
     }
 }
