18b58f00d02b154e3b708f74b49e0b0a5f9ccd75
AidanaIbrayeva <125108139+AidanaIbrayeva@users.noreply.github.com>
2023-02-16 19:48:09 +0600

    Update Database.java

--- src/Database.java
+++ src/Database.java
@@ -1,7 +1,8 @@
 package basic.atm;
 import java.sql.*;
+
 public class Database {
-  private Connection connection;
+    private Connection connection;
 
     public void connect() {
         try {
@@ -13,5 +14,29 @@ public class Database {
             e.printStackTrace();
         }
     }
+
+    public void disconnect() {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
   
+    public void saveAccount(Account account) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO accounts (first_name, last_name, card_number, pin_code, balance) VALUES (?, ?, ?, ?, ?)");
+            statement.setString(1, account.getFirstName());
+            statement.setString(2, account.getLastName());
+            statement.setString(3, account.getCardNumber());
+            statement.setString(4, account.getPincode());
+            statement.setInt(5, account.getBalance());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
