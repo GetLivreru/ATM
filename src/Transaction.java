1e3c4eec503c75470c2c4b4fea3a32794cb7f2e4
theadilet <110306539+GetLivreru@users.noreply.github.com>
2023-02-16 23:25:12 +0600

    Update Transaction.java

--- src/Transaction.java
+++ src/Transaction.java
@@ -3,4 +3,22 @@ package basic.atm;
 import java.time.LocalDateTime;
 
 public class Transaction {
+  private int amount;
+  private String transactionType;
+  private LocalDateTime transactionDateTime;
+  
+   public Transaction(int amount, String transactionType) {
+        this.amount = amount;
+        this.transactionType = transactionType;
+        this.transactionDateTime = LocalDateTime.now();
+    }
+   public int getAmount() {
+        return amount;
+    }
+    public String getTransactionType() {
+        return transactionType;
+    }
+   public LocalDateTime getTransactionDateTime() {
+        return transactionDateTime;
+    }
 }
