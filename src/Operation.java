7e135239fb77f4bfa232ea278e9e18ecad8670c9
Sepa <115605390+Qanysh@users.noreply.github.com>
2023-02-23 18:11:34 +0600

    Update Operation.java

--- src/Operation.java
+++ src/Operation.java
@@ -14,3 +14,23 @@ public class Operation {
         this.cardNumber = cardNumber;
         this.pincode = pincode;
     }
+    /*
+    Displaying the balance of the bank card
+    */
+    public Integer showBalance(String cardNumber) {
+        try {
+            Connection c = Database.connection();
+
+            Statement stmt5 = c.createStatement();
+
+            String sql5 = "SELECT * FROM balance WHERE card_number = " + cardNumber;
+
+            ResultSet rs5 = stmt5.executeQuery(sql5);
+
+            while (rs5.next()) {
+                this.balance = rs5.getInt(2);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
