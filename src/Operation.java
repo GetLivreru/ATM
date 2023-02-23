3e1076a38c1581531ab05d78ce60019a2ccc4195
Sepa <115605390+Qanysh@users.noreply.github.com>
2023-02-23 18:12:14 +0600

    Update Operation.java

--- src/Operation.java
+++ src/Operation.java
@@ -37,3 +37,44 @@ public class Operation {
 
         return this.balance;
     }
+    /*
+    Put money on a bank card
+    */
+    public void deposit(Integer amount, String cardNumber) {
+        try {
+            Connection c = Database.connection();
+
+            Statement stmt6 = c.createStatement();
+
+            String sql6 = "UPDATE balance SET balance = balance + '" + amount + "' WHERE card_number = " + cardNumber;
+
+            stmt6.executeUpdate(sql6);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    /*
+    Transfer money to another user
+    */
+    public void sendMoneyToOther(Integer amountOther, String numberOther, String cardNumber) {
+        try {
+            Connection c = Database.connection();
+
+            Statement stmt8 = c.createStatement();
+
+            String sql8 = "UPDATE balance SET balance = balance + " + amountOther + " WHERE card_number = '" + numberOther + "'";
+
+            stmt8.executeUpdate(sql8);
+
+
+            Statement stmt9 = c.createStatement();
+
+            String sql9 = "UPDATE balance SET balance = balance - " + amountOther + " WHERE card_number = '" + cardNumber + "'";
+
+            stmt9.executeUpdate(sql9);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
