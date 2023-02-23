db6912922e8b8cd7bd237621992050868b3dcee7
Sepa <115605390+Qanysh@users.noreply.github.com>
2023-02-23 18:31:57 +0600

    Update Database.java

--- src/Database.java
+++ src/Database.java
@@ -1,42 +1,21 @@
 package basic.atm;
+
 import java.sql.*;
 
+
 public class Database {
-    private Connection connection;
+    private static final String dbClassName = "com.mysql.cj.jdbc.Driver";
 
-    public void connect() {
-        try {
-            Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/bank", "postgres", "qwerty123");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+    private static final String CONNECTION = "jdbc:mysql://localhost:3306";
 
-    public void disconnect() {
-        try {
-            if (connection != null) {
-                connection.close();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-  
-    public void saveAccount(Account account) {
-        try {
-            PreparedStatement statement = connection.prepareStatement("INSERT INTO accounts (first_name, last_name, card_number, pin_code, balance) VALUES (?, ?, ?, ?, ?)");
-            statement.setString(1, account.getFirstName());
-            statement.setString(2, account.getLastName());
-            statement.setString(3, account.getCardNumber());
-            statement.setString(4, account.getPincode());
-            statement.setInt(5, account.getBalance());
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    private static final String USER = "Sepa";
+
+    private static final String PASSWORD = "1234";
+
+    public static java.sql.Connection connection() throws Exception {
+        Class.forName(dbClassName);
+
+        return DriverManager.getConnection(CONNECTION, USER, PASSWORD);
     }
 }
 
